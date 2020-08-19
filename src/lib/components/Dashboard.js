c7d0629e4c0e78929ce43138e15f18fae824d401
Mayur Borse <mayur@hyphenos.io>
2020-08-19 13:41:31 +0530

    Test cases passed for ErrorBoundary component
    - Added ErrorFallback component that gets rendered on error

--- src/lib/components/Dashboard.js
+++ src/lib/components/Dashboard.js
@@ -3,16 +3,17 @@
  *
  * @author Mayur Borse <mayur@hyphenos.io>
  */
-import React, { useState, useCallback } from 'react'
-import defaultConfig from '../constants/defaultConfig'
-import Table from './Table'
-import PacketDetailsViewer from './PacketDetailsViewer'
+import React, { useState, useCallback } from "react";
+import defaultConfig from "../constants/defaultConfig";
+import Table from "./Table";
+import PacketDetailsViewer from "./PacketDetailsViewer";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Dashboard = ({ packets, config }) => {
   /**
    * selectedPacket will be rendered by PacketDetailsViewer if not empty
    */
-  const [selectedPacket, setSelectedPacket] = useState(null)
+  const [selectedPacket, setSelectedPacket] = useState(null);
 
   /**
    * Toggles selected packet between received packet object or empty object {}
@@ -20,48 +21,50 @@ const Dashboard = ({ packets, config }) => {
    * useCallback hook invokes this functions only when packet is selected in Table.
    */
   const getSelectedPacket = useCallback((packet) => {
-    setSelectedPacket(selectedPacket ? null : packet)
-  }, [])
+    setSelectedPacket(selectedPacket ? null : packet);
+  }, []);
 
   /**
    *  Current config state
    */
   const [currentConfig, setCurrentConfig] = useState(() =>
     config ? mergeConfig(defaultConfig, config) : defaultConfig
-  )
+  );
 
   function mergeConfig(oldConfig, newConfig) {
-    let mergedConfig = {}
+    let mergedConfig = {};
     for (let key in newConfig) {
-      mergedConfig[key] = { ...oldConfig[key], ...newConfig[key] }
+      mergedConfig[key] = { ...oldConfig[key], ...newConfig[key] };
     }
-    return mergedConfig
+    return mergedConfig;
   }
 
   /**
    * Config objects are passed to respective components.
    */
-  const { dashboardConfig, tableConfig, detailsConfig } = currentConfig
+  const { dashboardConfig, tableConfig, detailsConfig } = currentConfig;
 
-  if (!packets) return <h2>No packets provided</h2>
+  if (!packets) return <h2>No packets provided</h2>;
 
   return (
-    <div className='packet-dashboard'>
-      {packets && (
-        <Table
-          getSelectedPacket={getSelectedPacket}
-          packets={packets}
-          config={tableConfig}
-        />
-      )}
+    <div className="packet-dashboard">
+      <ErrorBoundary>
+        {packets && (
+          <Table
+            getSelectedPacket={getSelectedPacket}
+            packets={packets}
+            config={tableConfig}
+          />
+        )}
 
-      {dashboardConfig?.showSelectedDetails && selectedPacket && (
-        <PacketDetailsViewer
-          selectedPacket={selectedPacket}
-          config={detailsConfig}
-        />
-      )}
+        {dashboardConfig?.showSelectedDetails && selectedPacket && (
+          <PacketDetailsViewer
+            selectedPacket={selectedPacket}
+            config={detailsConfig}
+          />
+        )}
+      </ErrorBoundary>
     </div>
-  )
-}
-export default Dashboard
+  );
+};
+export default Dashboard;
